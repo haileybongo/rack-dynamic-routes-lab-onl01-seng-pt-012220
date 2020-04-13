167bcc5ea957867984a8383e05c02bad440fb6bc
Hailey Bongo <haileyebongo@gmail.com>
2020-04-13 18:10:58 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,29 @@
+require 'pry'
+
+class Application
+  
+  
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    
+    if req.path.match(/items/)
+      req_item = req.params["items"]
+      binding.pry 
+      
+      if Item.all.select(name = req_item)
+        item = Item.all.select(name = req_item)
+        resp.write "#{item.price}\n"
+      else 
+        resp.write "Item not found"
+        resp.status = 400
+      end
+    else 
+      resp.write "Route not found"
+      resp.status = 404
+    end
+    resp.finish
+  end
+
+end
